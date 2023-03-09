8b44aef95dfeef5f844391d02d13246ceaded62c
_311551169_劉宇承 <laxiflora@LAPTOP-95KBHVNS.localdomain>
2023-03-09 17:36:40 +0800

    Lab03_upload

--- Lab03/calculator_test.py
+++ Lab03/calculator_test.py
@@ -10,23 +10,27 @@ class ApplicationTest(unittest.TestCase):
     def test_add(self):
         for i, j in para:
             self.assertEqual(Calculator.add(i, j), i+j)
+        with self.assertRaises(TypeError):
+            Calculator.add(1, "2")
 
-        self.assertRaises(TypeError, Calculator.add(1, "2"))
 
     def test_divide(self):
         for i, j in para:
             self.assertEqual(Calculator.divide(i, j), i/j)
-        self.assertRaises(TypeError, Calculator.divide("1", "2"))
+        with self.assertRaises(TypeError):
+            Calculator.divide(1, "2")
 
     def test_sqrt(self):
         for i, j in para:
             self.assertEqual(Calculator.sqrt(i), sqrt(i))
-        self.assertRaises(TypeError, Calculator.sqrt("4"))
+        with self.assertRaises(TypeError):
+            Calculator.sqrt(1, "2")
 
     def test_exp(self):
         for i, j in para:
             self.assertEqual(Calculator.exp(i), exp(i))
-        self.assertRaises(TypeError, Calculator.exp("1"))
+        with self.assertRaises(TypeError):
+            Calculator.exp(1, "2")
 
 
 if __name__ == '__main__':
