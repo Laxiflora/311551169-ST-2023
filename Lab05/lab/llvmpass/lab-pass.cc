b7fd18a8e931235b916b0176c06e67ffe747a36d
_311551169_劉宇承 <laxiflora@LAPTOP-95KBHVNS.localdomain>
2023-03-31 17:21:22 +0800

    lab5_10%

--- Lab05/lab/llvmpass/lab-pass.cc
+++ Lab05/lab/llvmpass/lab-pass.cc
@@ -17,12 +17,60 @@ bool LabPass::doInitialization(Module &M) {
   return true;
 }
 
+
+static FunctionCallee printfPrototype(Module &M) {
+  LLVMContext &ctx = M.getContext();
+
+  FunctionType *printfType = FunctionType::get(
+    Type::getInt32Ty(ctx),
+    { Type::getInt8PtrTy(ctx) },
+    true);
+
+  FunctionCallee printfCallee = M.getOrInsertFunction("printf", printfType);
+
+  return printfCallee;
+}
+
+static FunctionCallee exitPrototype(Module &M) {
+  LLVMContext &ctx = M.getContext();
+
+  FunctionType *exitType = FunctionType::get(
+    Type::getInt32Ty(ctx),
+    { Type::getInt32Ty(ctx) },
+    false);
+
+  FunctionCallee exitCallee = M.getOrInsertFunction("exit", exitType);
+
+  return exitCallee;
+}
+
+
+
+
+
+
+static void dumpIR(Function &F)
+{
+  for (auto &BB : F) {
+    errs() << "BB: " << "\n";
+    errs() << BB << "\n";
+  }
+}
+
 bool LabPass::runOnModule(Module &M) {
   errs() << "runOnModule\n";
 
-  for (auto &F : M) {
-    errs() << F.getName() << "\n";
+  // get printf and exit() function for creating call
+  FunctionCallee exitCallee = exitPrototype(M);
+  FunctionCallee printfCallee = printfPrototype(M);
 
+
+    //BuilderBof.CreateCall(printfCallee, { stackBofMsg });
+    //BuilderBof.CreateCall(exitCallee, { one });
+  for (auto &F : M) {
+    errs() << F.getName() << " " << &F.getEntryBlock().front() << "\n";
+    errs()<< getDepth(M,F) << "\n";
+    dumpIR(F);
     // TODO
   }
 
